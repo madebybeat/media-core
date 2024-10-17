a782672f9f3435ba71114de1d6811eeb187c72dc
Ailakks <hola@ailakks.dev>
2024-10-17 21:33:51 +0200

    feat: http request load indicator

---
+++ core/src/main/java/com/beatstreaming/core/ui/LoadIndicator.java
@@ -0,0 +1,10 @@
+package com.beatstreaming.core.ui;
+
+import android.content.Context;
+import android.widget.ProgressBar;
+
+public class LoadIndicator extends ProgressBar {
+    public LoadIndicator(Context context) {
+        super(context);
+    }
+}
