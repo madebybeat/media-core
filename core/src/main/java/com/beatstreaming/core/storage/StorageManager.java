23c8827e379470ef848526ef8b6bd4a969e243a5
Ailakks <hola@ailakks.dev>
2024-10-17 23:25:41 +0200

    feat: storage manager

---
+++ core/src/main/java/com/beatstreaming/core/storage/StorageManager.java
@@ -0,0 +1,54 @@
+package com.beatstreaming.core.storage;
+
+import android.content.Context;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+
+import lombok.SneakyThrows;
+
+public class StorageManager<T> {
+    private final Class<T> clazz;
+    private final String name;
+
+    private final Gson gson;
+
+    public StorageManager(Class<T> clazz, String name) {
+        this.clazz = clazz;
+        this.name = name;
+
+        this.gson = new Gson();
+    }
+
+    private String getName() {
+        return this.name + ".json";
+    }
+
+    @SneakyThrows
+    public T load(Context context) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        FileInputStream fileInputStream = context.openFileInput(this.getName());
+        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String line;
+
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuilder.append(line);
+        }
+
+        return this.gson.fromJson(stringBuilder.toString(), this.clazz);
+    }
+
+    @SneakyThrows
+    public void save(Context context, T object) {
+        FileOutputStream fileOutputStream = context.openFileOutput(this.getName(), Context.MODE_PRIVATE);
+
+        fileOutputStream.write(this.gson.toJson(object).getBytes());
+    }
+}
