98e16030fcabdb31330d52318ed30edcabcd9576
Ailakks <hola@ailakks.dev>
2024-10-18 00:41:54 +0200

    feat: app source binder layout

--- media/src/main/java/com/beatstreaming/media/list/AppSourceBinder.java
+++ media/src/main/java/com/beatstreaming/media/list/AppSourceBinder.java
@@ -1,8 +1,22 @@
 package com.beatstreaming.media.list;
 
+import android.widget.TextView;
+
 import com.beatstreaming.core.list.ListBinder;
 import com.beatstreaming.core.list.ListContext;
+import com.beatstreaming.core.list.ListViewHolder;
+import com.beatstreaming.media.R;
 import com.beatstreaming.media.entity.AppSourceEntity;
 
 public class AppSourceBinder extends ListBinder<ListContext, AppSourceEntity> {
+    @Override
+    public void bind(ListContext context, ListViewHolder<AppSourceEntity> holder, AppSourceEntity item) {
+        TextView appName = holder.itemView.findViewById(R.id.app_name);
+
+        appName.setText(item.getName());
+    }
+
+    public int getItemLayout() {
+        return R.layout.app_source_binder;
+    }
 }
