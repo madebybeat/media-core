3a3a674d2c3c340c227c7405490677d91f9f70fc
Ailakks <hola@ailakks.dev>
2024-10-17 20:30:13 +0200

    feat: app source entity

---
+++ core/src/main/java/com/beatstreaming/core/entity/AppSourceEntity.java
@@ -0,0 +1,11 @@
+package com.beatstreaming.core.entity;
+
+import lombok.Data;
+
+@Data
+public class AppSourceEntity {
+    private final String url;
+    private final String description;
+    private final String name;
+    private final String website;
+}
