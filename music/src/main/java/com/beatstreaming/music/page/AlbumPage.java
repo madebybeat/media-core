c0d08b7df238cb0aac8bd10c23a00edd6f8e8d32
Ailakks <hola@ailakks.dev>
2024-10-20 20:56:18 +0200

    feat: collection page

---
+++ music/src/main/java/com/beatstreaming/music/page/AlbumPage.java
@@ -0,0 +1,40 @@
+package com.beatstreaming.music.page;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.beatstreaming.media.AppSourceContext;
+import com.beatstreaming.media.databinding.CollectionPageBinding;
+import com.beatstreaming.music.entity.AlbumEntity;
+import com.squareup.picasso.Picasso;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class AlbumPage extends Fragment {
+    private CollectionPageBinding collectionPageBinding;
+
+    private final AppSourceContext appSourceContext;
+    private final AlbumEntity albumEntity;
+
+    public AlbumPage(AppSourceContext appSourceContext, AlbumEntity albumEntity) {
+        this.appSourceContext = appSourceContext;
+        this.albumEntity = albumEntity;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle bundle) {
+        this.collectionPageBinding = CollectionPageBinding.inflate(this.getLayoutInflater());
+
+        this.collectionPageBinding.collectionName.setText(this.albumEntity.getName());
+
+        Picasso.get().load(this.albumEntity.getImage().getUrl()).into(this.collectionPageBinding.collectionCover.trackImage);
+
+        return this.collectionPageBinding.getRoot();
+    }
+}
