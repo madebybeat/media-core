bdfc99b2ea1bb446c8af2fc4a75eeac2aa94f8b6
Ailakks <hola@ailakks.dev>
2024-10-22 19:00:04 +0200

    feat: base player

---
+++ media/src/main/java/com/beatstreaming/media/player/BasePlayer.java
@@ -0,0 +1,49 @@
+package com.beatstreaming.media.player;
+
+import android.content.Context;
+
+import androidx.media3.exoplayer.ExoPlayer;
+import androidx.media3.session.MediaSession;
+
+import com.beatstreaming.core.event.Listener;
+
+import java.net.URI;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class BasePlayer extends Listener<BasePlayer, PlayerCallback> {
+    public final Context context;
+
+    protected ExoPlayer player;
+    protected MediaSession mediaSession;
+
+    @Setter
+    private boolean isLoading;
+
+    public BasePlayer(Context context, PlayerContext playerContext) {
+        super(BasePlayer.class, PlayerCallback.class);
+
+        this.context = context;
+
+        this.playerContext = playerContext;
+    }
+
+    public void onInit() {
+        this.mediaSession = new MediaSession.Builder(this.context, this.player).build();
+
+        this.callEvent(PlayerCallback::onInit);
+    }
+
+    public void load() {
+
+    }
+
+    public void onLoad() {
+
+    }
+
+    public void start(URI uri) {
+    }
+}
