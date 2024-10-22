fe988714459b0a645d2e6b02fcf9dcf12985cacf
Ailakks <hola@ailakks.dev>
2024-10-22 19:44:18 +0200

    feat: player menu button

---
+++ media/src/main/java/com/beatstreaming/media/player/ui/PlayerMenuButton.java
@@ -0,0 +1,18 @@
+package com.beatstreaming.media.player.ui;
+
+import android.content.Context;
+
+import com.beatstreaming.core.ui.ImageButton;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class PlayerMenuButton extends ImageButton {
+    public PlayerMenuButton(Context context) {
+        super(context);
+    }
+
+    public void init(Context context) {
+        super.init(context);
+    }
+}
