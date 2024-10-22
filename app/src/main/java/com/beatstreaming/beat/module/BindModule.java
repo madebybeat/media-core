19c3fa7ea0ef1e141db99acfae58c9b6f0ccbd14
Ailakks <hola@ailakks.dev>
2024-10-23 00:03:18 +0200

    fix: player binding

--- app/src/main/java/com/beatstreaming/beat/module/BindModule.java
+++ app/src/main/java/com/beatstreaming/beat/module/BindModule.java
@@ -1,5 +1,7 @@
 package com.beatstreaming.beat.module;
 
+import android.content.Context;
+
 import com.beatstreaming.beat.item.AlbumCardImageItemBinder;
 import com.beatstreaming.beat.item.AppAlbumItemType;
 import com.beatstreaming.beat.item.AppArtistItemType;
@@ -11,17 +13,21 @@ import com.beatstreaming.beat.page.AppPages;
 import com.beatstreaming.beat.server.DefaultAppServerManager;
 import com.beatstreaming.core.pages.HomePage;
 import com.beatstreaming.core.pages.Pages;
+import com.beatstreaming.media.player.Player;
+import com.beatstreaming.media.player.ui.PlayerBar;
 import com.beatstreaming.media.server.AppServerManager;
 import com.beatstreaming.music.item.AbstractLibraryItemBinder;
 import com.beatstreaming.music.item.AlbumItemType;
 import com.beatstreaming.music.item.ArtistItemType;
 import com.beatstreaming.music.player.MusicPlayer;
+import com.beatstreaming.music.player.ui.TrackPlayerBar;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
+import dagger.hilt.android.qualifiers.ApplicationContext;
 import dagger.hilt.components.SingletonComponent;
 
 @Module
@@ -85,5 +91,17 @@ public class BindModule {
     @Singleton
     public ArtistItemType provideArtistItemType() {
         return new AppArtistItemType();
+    }
+
+    @Provides
+    @Singleton
+    public Player<?> providePlayer(@ApplicationContext Context context) {
+        return new MusicPlayer(context);
+    }
+
+    @Provides
+    @Singleton
+    public PlayerBar<?> providePlayerBar(MusicPlayer player) {
+        return new TrackPlayerBar(player);
     }
 }
--- music/src/main/java/com/beatstreaming/music/module/BindModule.java
+++ music/src/main/java/com/beatstreaming/music/module/BindModule.java
@@ -1,19 +1,12 @@
 package com.beatstreaming.music.module;
 
-import android.content.Context;
-
-import com.beatstreaming.media.player.Player;
-import com.beatstreaming.media.player.ui.PlayerBar;
 import com.beatstreaming.music.item.AbstractLibraryItemBinder;
-import com.beatstreaming.music.player.MusicPlayer;
-import com.beatstreaming.music.player.ui.TrackPlayerBar;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
-import dagger.hilt.android.qualifiers.ApplicationContext;
 import dagger.hilt.components.SingletonComponent;
 
 @Module
@@ -23,17 +16,5 @@ public class BindModule {
     @Singleton
     public AbstractLibraryItemBinder provideAbstractLibraryItemBinder() {
         return new AbstractLibraryItemBinder();
-    }
-
-    @Provides
-    @Singleton
-    public Player<?> providePlayer(@ApplicationContext Context context) {
-        return new MusicPlayer(context);
-    }
-
-    @Provides
-    @Singleton
-    public PlayerBar<?> providePlayerBar(MusicPlayer player) {
-        return new TrackPlayerBar(player);
     }
 }
