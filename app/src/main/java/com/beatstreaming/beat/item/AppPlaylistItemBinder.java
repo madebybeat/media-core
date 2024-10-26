154cdf71284f55fc016f292e8a17eded44c00177
Ailakks <hola@ailakks.dev>
2024-10-26 19:13:35 +0200

    Revert "perf: playlist item binder"
    This reverts commit 852deea58c52b626a4631e8f782abfbe591c621e.

--- app/src/main/java/com/beatstreaming/beat/item/AppPlaylistItemBinder.java
+++ app/src/main/java/com/beatstreaming/beat/item/AppPlaylistItemBinder.java
@@ -5,14 +5,13 @@ import android.view.View;
 import com.beatstreaming.beat.page.AppPlaylistPage;
 import com.beatstreaming.core.MainActivity;
 import com.beatstreaming.core.list.ListViewHolder;
-import com.beatstreaming.media.storage.library.LibraryItemEntity;
 import com.beatstreaming.music.entity.PlaylistEntity;
 import com.beatstreaming.music.item.playlist.PlaylistItemBinder;
 import com.beatstreaming.music.list.TrackListContext;
 
 public class AppPlaylistItemBinder extends PlaylistItemBinder {
     @Override
-    public void bind(TrackListContext context, ListViewHolder<LibraryItemEntity<PlaylistEntity>> holder, LibraryItemEntity<PlaylistEntity> item) {
+    public void bind(TrackListContext context, ListViewHolder<PlaylistEntity> holder, PlaylistEntity item) {
         super.bind(context, holder, item);
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
@@ -20,7 +19,7 @@ public class AppPlaylistItemBinder extends PlaylistItemBinder {
             public void onClick(View view) {
                 MainActivity.mainActivity.getSupportFragmentManager()
                         .beginTransaction()
-                        .replace(MainActivity.mainActivity.getMainActivityBinding().fragment.getId(), new AppPlaylistPage(null, item.getItem()))
+                        .replace(MainActivity.mainActivity.getMainActivityBinding().fragment.getId(), new AppPlaylistPage(null, item))
                         .addToBackStack(null)
                         .commit();
             }
--- music/src/main/java/com/beatstreaming/music/item/playlist/PlaylistItemBinder.java
+++ music/src/main/java/com/beatstreaming/music/item/playlist/PlaylistItemBinder.java
@@ -5,22 +5,21 @@ import android.view.LayoutInflater;
 import com.beatstreaming.core.list.ListViewHolder;
 import com.beatstreaming.media.databinding.MediaCoverImageBinding;
 import com.beatstreaming.media.list.ItemListItemBinder;
-import com.beatstreaming.media.storage.library.LibraryItemEntity;
 import com.beatstreaming.music.R;
 import com.beatstreaming.music.entity.PlaylistEntity;
 import com.beatstreaming.music.list.TrackListContext;
 import com.squareup.picasso.Picasso;
 
-public class PlaylistItemBinder extends ItemListItemBinder<TrackListContext, LibraryItemEntity<PlaylistEntity>> {
+public class PlaylistItemBinder extends ItemListItemBinder<TrackListContext, PlaylistEntity> {
     @Override
-    public void bind(TrackListContext context, ListViewHolder<LibraryItemEntity<PlaylistEntity>> holder, LibraryItemEntity<PlaylistEntity> item) {
+    public void bind(TrackListContext context, ListViewHolder<PlaylistEntity> holder, PlaylistEntity item) {
         super.bind(context, holder, item);
 
-        this.mediaTitle.setText(item.getItem().getName());
-        this.mediaSubtitle.setText(String.format(holder.itemView.getContext().getResources().getText(item.getItem().getTracks().size() == 1 ? R.string.playlist_description_singular : R.string.playlist_description_plural).toString(), item.getItem().getTracks().size()));
+        this.mediaTitle.setText(item.getName());
+        this.mediaSubtitle.setText(String.format(holder.itemView.getContext().getResources().getText(item.getTracks().size() == 1 ? R.string.playlist_description_singular : R.string.playlist_description_plural).toString(), item.getTracks().size()));
 
         MediaCoverImageBinding mediaCoverImageBinding = MediaCoverImageBinding.inflate(LayoutInflater.from(this.mediaCover.getContext()));
-        Picasso.get().load(item.getItem().getImage().getUrl()).into(mediaCoverImageBinding.mediaImage);
+        Picasso.get().load(item.getImage().getUrl()).into(mediaCoverImageBinding.mediaImage);
 
         this.mediaCover.addView(mediaCoverImageBinding.getRoot());
     }
