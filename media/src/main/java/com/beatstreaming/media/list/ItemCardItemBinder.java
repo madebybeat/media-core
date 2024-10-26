204af1bf84650111e407cb694ca682e3857808ae
Ailakks <hola@ailakks.dev>
2024-10-26 15:03:45 +0200

    feat: item card binder

---
+++ media/src/main/java/com/beatstreaming/media/list/ItemCardItemBinder.java
@@ -0,0 +1,28 @@
+package com.beatstreaming.media.list;
+
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.beatstreaming.core.entity.ItemEntity;
+import com.beatstreaming.core.list.ListBinder;
+import com.beatstreaming.core.list.ListContext;
+import com.beatstreaming.core.list.ListViewHolder;
+import com.beatstreaming.media.R;
+
+public class ItemCardItemBinder<T extends ListContext, V extends ItemEntity> extends ListBinder<T, V> {
+    protected LinearLayout mediaCover;
+    protected TextView mediaTitle;
+    protected TextView mediaSubtitle;
+
+    @Override
+    public void bind(T context, ListViewHolder<V> holder, V item) {
+        this.mediaCover = holder.itemView.findViewById(R.id.media_cover);
+        this.mediaTitle = holder.itemView.findViewById(R.id.media_title);
+        this.mediaSubtitle = holder.itemView.findViewById(R.id.media_subtitle);
+    }
+
+    @Override
+    public int getItemLayout() {
+        return R.layout.media_card_item_binder;
+    }
+}
--- media/src/main/java/com/beatstreaming/media/list/MediaCardItemBinder.java
+++ media/src/main/java/com/beatstreaming/media/list/MediaCardItemBinder.java
@@ -1,28 +1,7 @@
 package com.beatstreaming.media.list;
 
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
 import com.beatstreaming.core.entity.ItemEntity;
-import com.beatstreaming.core.list.ListBinder;
-import com.beatstreaming.core.list.ListViewHolder;
 import com.beatstreaming.media.AppSourceContext;
-import com.beatstreaming.media.R;
 
-public class MediaCardItemBinder<T extends ItemEntity> extends ListBinder<AppSourceContext, T> {
-    protected LinearLayout mediaCover;
-    protected TextView mediaTitle;
-    protected TextView mediaSubtitle;
-
-    @Override
-    public void bind(AppSourceContext context, ListViewHolder<T> holder, T item) {
-        this.mediaCover = holder.itemView.findViewById(R.id.media_cover);
-        this.mediaTitle = holder.itemView.findViewById(R.id.media_title);
-        this.mediaSubtitle = holder.itemView.findViewById(R.id.media_subtitle);
-    }
-
-    @Override
-    public int getItemLayout() {
-        return R.layout.media_card_item_binder;
-    }
+public class MediaCardItemBinder<T extends ItemEntity> extends ItemCardItemBinder<AppSourceContext, T> {
 }
