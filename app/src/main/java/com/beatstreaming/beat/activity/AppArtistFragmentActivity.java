73e504bde48de9dfb0f23f15c07b58e450bca675
Ailakks <hola@ailakks.dev>
2024-10-27 13:00:53 +0100

    perf: app artist fragment activity

--- app/src/main/java/com/beatstreaming/beat/activity/AppArtistFragmentActivity.java
+++ app/src/main/java/com/beatstreaming/beat/activity/AppArtistFragmentActivity.java
@@ -1,14 +1,15 @@
 package com.beatstreaming.beat.activity;
 
 import com.beatstreaming.beat.page.AppArtistPage;
-import com.beatstreaming.core.activity.SerializableFragmentMainActivity;
+import com.beatstreaming.core.activity.FragmentMainActivity;
+import com.beatstreaming.media.list.AppSourceListContext;
 import com.beatstreaming.music.entity.ArtistEntity;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
-public class AppArtistFragmentActivity extends SerializableFragmentMainActivity<ArtistEntity> {
-    public AppArtistFragmentActivity() {
-        super(AppArtistPage.class, ArtistEntity.class);
+public class AppArtistFragmentActivity extends FragmentMainActivity {
+    public AppArtistFragmentActivity(AppSourceListContext appSourceListContext, ArtistEntity artistEntity) {
+        super(AppArtistPage.class, new Class[]{AppSourceListContext.class, ArtistEntity.class}, new Object[]{appSourceListContext, artistEntity});
     }
 }
