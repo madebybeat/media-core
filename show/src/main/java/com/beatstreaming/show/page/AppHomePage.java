ad3788a37df3d7af9a7533e73f5402653c6d255d
Ailakks <hola@ailakks.dev>
2024-10-28 23:27:12 +0100

    feat: home page

---
+++ show/src/main/java/com/beatstreaming/show/page/AppHomePage.java
@@ -0,0 +1,25 @@
+package com.beatstreaming.show.page;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+
+import com.beatstreaming.core.pages.HomePage;
+import com.beatstreaming.show.databinding.HomePageBinding;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class AppHomePage extends HomePage {
+    private HomePageBinding homePageBinding;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle bundle) {
+        this.homePageBinding = HomePageBinding.inflate(this.getLayoutInflater());
+
+        return this.homePageBinding.getRoot();
+    }
+}
