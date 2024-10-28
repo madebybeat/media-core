448be54e04a6ec1e62d25c0a22ff3d36a24d41e2
Ailakks <hola@ailakks.dev>
2024-10-28 23:27:39 +0100

    feat: bind module

---
+++ show/src/main/java/com/beatstreaming/show/module/BindModule.java
@@ -0,0 +1,21 @@
+package com.beatstreaming.show.module;
+
+import com.beatstreaming.media.server.AppServerManager;
+import com.beatstreaming.show.server.DefaultAppServerManager;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.components.SingletonComponent;
+
+@Module
+@InstallIn(SingletonComponent.class)
+public class BindModule {
+    @Provides
+    @Singleton
+    public AppServerManager provideAppServerManager() {
+        return new DefaultAppServerManager();
+    }
+}
