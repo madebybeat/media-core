6c9c02dd381802bd1398f42e0a8ce8cec5404d8b
Ailakks <hola@ailakks.dev>
2024-10-29 00:09:39 +0100

    feat: latest movie section home impl

--- show/src/main/java/com/beatstreaming/show/page/AppHomePage.java
+++ show/src/main/java/com/beatstreaming/show/page/AppHomePage.java
@@ -8,9 +8,14 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 
 import com.beatstreaming.core.pages.HomePage;
+import com.beatstreaming.media.storage.app.AppSourceStorageItem;
+import com.beatstreaming.media.storage.app.AppSourceStorageManager;
 import com.beatstreaming.show.databinding.HomePageBinding;
+import com.beatstreaming.show.databinding.HomeSectionBinding;
+import com.beatstreaming.show.http.LatestMovieListRequest;
+import com.beatstreaming.show.item.MovieCardItemBinder;
 
-import java.util.Arrays;
+import javax.inject.Inject;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
@@ -18,9 +23,17 @@ import dagger.hilt.android.AndroidEntryPoint;
 public class AppHomePage extends HomePage {
     private HomePageBinding homePageBinding;
 
+    @Inject AppSourceStorageManager appSourceStorageManager;
+
+    @Inject MovieCardItemBinder movieCardItemBinder;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle bundle) {
         this.homePageBinding = HomePageBinding.inflate(this.getLayoutInflater());
+
+        AppSourceStorageItem appSourceStorageItem = this.appSourceStorageManager.load(this.getContext());
+
+        new LatestMovieListRequest(this.getLayoutInflater().getContext(), this.homePageBinding, appSourceStorageItem.getAppSourceEntity(), HomeSectionBinding.inflate(this.getLayoutInflater()), this.movieCardItemBinder);
 
         return this.homePageBinding.getRoot();
     }
