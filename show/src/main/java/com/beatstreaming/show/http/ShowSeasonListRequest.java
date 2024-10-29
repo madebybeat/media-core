3c826615bd05696bcc3d839f5a6fc81f9264688a
Ailakks <hola@ailakks.dev>
2024-10-30 00:24:45 +0100

    feat: show season list request

---
+++ show/src/main/java/com/beatstreaming/show/http/ShowSeasonListRequest.java
@@ -0,0 +1,43 @@
+package com.beatstreaming.show.http;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.beatstreaming.core.http.HttpRequestBinding;
+import com.beatstreaming.media.entity.AppSourceEntity;
+import com.beatstreaming.media.list.AppSourceListContext;
+import com.beatstreaming.show.databinding.HomeSectionBinding;
+import com.beatstreaming.show.databinding.TitlePageBinding;
+import com.beatstreaming.show.entity.SeasonEntityList;
+import com.beatstreaming.show.entity.TitleEntity;
+import com.beatstreaming.show.item.ShowSeasonListItemBinder;
+import com.beatstreaming.show.section.ShowSeasonSectionContext;
+
+import org.apache.http.client.utils.URIBuilder;
+
+import lombok.SneakyThrows;
+
+public class ShowSeasonListRequest extends HttpRequestBinding<SeasonEntityList, TitlePageBinding, HomeSectionBinding> {
+    private final AppSourceEntity appSourceEntity;
+    private final ShowSeasonListItemBinder showSeasonListItemBinder;
+
+    @SneakyThrows
+    public ShowSeasonListRequest(Context context, TitlePageBinding titlePageBinding, AppSourceEntity appSourceEntity, TitleEntity titleEntity, HomeSectionBinding homeSectionBinding, ShowSeasonListItemBinder seasonListItemBinder) {
+        super(context, titlePageBinding.platformSection, titlePageBinding, homeSectionBinding, SeasonEntityList.class, Request.Method.GET);
+
+        this.appSourceEntity = appSourceEntity;
+        this.showSeasonListItemBinder = seasonListItemBinder;
+
+        this.load(new URIBuilder(this.appSourceEntity.getUrl())
+                .setPathSegments("api", "v1", "season", titleEntity.getId())
+                .addParameter("type", titleEntity.getType().toString())
+                .build());
+    }
+
+    @Override
+    public void onLoad(SeasonEntityList seasonEntityList) {
+        this.resultBinding.section.init(new ShowSeasonSectionContext(this.context, new AppSourceListContext(this.appSourceEntity), seasonEntityList.getList(), this.showSeasonListItemBinder));
+
+        super.onLoad(seasonEntityList);
+    }
+}
