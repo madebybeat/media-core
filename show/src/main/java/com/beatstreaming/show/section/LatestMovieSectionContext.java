59c0f1a05e48312926f1c481050a2cd786cc9e27
Ailakks <hola@ailakks.dev>
2024-10-29 21:36:59 +0100

    perf: title binder style

--- show/src/main/java/com/beatstreaming/show/section/LatestMovieSectionContext.java
+++ show/src/main/java/com/beatstreaming/show/section/LatestMovieSectionContext.java
@@ -2,6 +2,8 @@ package com.beatstreaming.show.section;
 
 import android.content.Context;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+
 import com.beatstreaming.core.component.list.ListSectionContext;
 import com.beatstreaming.core.list.ListRecyclerViewAdapter;
 import com.beatstreaming.media.list.AppSourceListContext;
@@ -12,5 +14,10 @@ import com.beatstreaming.show.item.MovieCardItemBinder;
 public class LatestMovieSectionContext extends ListSectionContext<AppSourceListContext, MovieEntity> {
     public LatestMovieSectionContext(Context context, AppSourceListContext appSourceContext, MovieEntity[] entities, MovieCardItemBinder binder) {
         super(context, R.string.section_movie_latest_name, new ListRecyclerViewAdapter<AppSourceListContext, MovieEntity>(appSourceContext, entities, binder));
+    }
+
+    @Override
+    public void onBind() {
+        this.listSectionBinding.sectionList.setLayoutManager(new LinearLayoutManager(this.context, LinearLayoutManager.HORIZONTAL, false));
     }
 }
