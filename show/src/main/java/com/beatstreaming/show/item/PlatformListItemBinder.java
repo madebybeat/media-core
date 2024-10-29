ae9bc94882f840f22125bcb291eb927c0b359e88
Ailakks <hola@ailakks.dev>
2024-10-30 00:15:56 +0100

    feat: platform section context

---
+++ show/src/main/java/com/beatstreaming/show/item/PlatformListItemBinder.java
@@ -0,0 +1,13 @@
+package com.beatstreaming.show.item;
+
+import com.beatstreaming.core.list.ListViewHolder;
+import com.beatstreaming.media.item.ListImageItemBinder;
+import com.beatstreaming.media.list.AppSourceListContext;
+import com.beatstreaming.show.entity.PlatformEntity;
+
+public class PlatformListItemBinder extends ListImageItemBinder<PlatformEntity> {
+    @Override
+    public void bind(AppSourceListContext context, ListViewHolder<PlatformEntity> holder, PlatformEntity item) {
+        super.bind(context, holder, item);
+    }
+}
