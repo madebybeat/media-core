02d43c688467d5b08a50ccc2a79e40417fe57449
Ailakks <hola@ailakks.dev>
2024-11-05 20:08:04 +0100

    Revert "perf: player context super"
    This reverts commit 33f5225920b01e2168862fc52b4685ff528343e3.

--- media/src/main/java/com/beatstreaming/media/player/PlayerContext.java
+++ media/src/main/java/com/beatstreaming/media/player/PlayerContext.java
@@ -3,7 +3,8 @@ package com.beatstreaming.media.player;
 import androidx.media3.common.MediaItem;
 
 import com.beatstreaming.core.entity.ItemEntity;
-import com.beatstreaming.core.list.ItemListContext;
+import com.beatstreaming.media.entity.AppSourceEntity;
+import com.beatstreaming.media.list.AppSourceListContext;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,11 +14,11 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class PlayerContext<T extends ItemEntity, V extends ItemEntity> extends ItemListContext<V> {
+public class PlayerContext<T extends ItemEntity, V extends ItemEntity> extends AppSourceListContext {
     protected final PlayerSource<T, V> playerSource;
 
-    public PlayerContext(V item, PlayerSource<T, V> playerSource) {
-        super(item);
+    public PlayerContext(AppSourceEntity appSourceEntity, PlayerSource<T, V> playerSource) {
+        super(appSourceEntity);
 
         this.playerSource = playerSource;
     }
