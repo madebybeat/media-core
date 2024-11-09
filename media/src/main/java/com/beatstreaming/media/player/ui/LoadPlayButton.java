b92c40fb58fccb1c7fe7d62150d62b80a568e496
Ailakks <hola@ailakks.dev>
2024-11-09 18:02:27 +0100

    feat: load play status

--- media/src/main/java/com/beatstreaming/media/player/ui/LoadPlayButton.java
+++ media/src/main/java/com/beatstreaming/media/player/ui/LoadPlayButton.java
@@ -2,13 +2,9 @@ package com.beatstreaming.media.player.ui;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.View;
 
-import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.core.content.ContextCompat;
 
-import com.beatstreaming.core.view.ItemRefresh;
-import com.beatstreaming.core.view.ItemInit;
 import com.beatstreaming.media.R;
 import com.beatstreaming.media.player.Player;
 import com.beatstreaming.media.player.PlayerCallback;
@@ -18,22 +14,22 @@ import javax.inject.Inject;
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
-public class PlayButton extends AppCompatImageButton implements ItemInit<Context>, ItemRefresh {
+public class LoadPlayButton extends PlayButton {
     @Inject Player<?> player;
 
-    public PlayButton(Context context) {
+    public LoadPlayButton(Context context) {
         super(context);
 
         this.init(context);
     }
 
-    public PlayButton(Context context, AttributeSet attributeSet) {
+    public LoadPlayButton(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
 
         this.init(context);
     }
 
-    public PlayButton(Context context, AttributeSet attributeSet, int defaultStyleAttributes) {
+    public LoadPlayButton(Context context, AttributeSet attributeSet, int defaultStyleAttributes) {
         super(context, attributeSet, defaultStyleAttributes);
 
         this.init(context);
@@ -44,26 +40,18 @@ public class PlayButton extends AppCompatImageButton implements ItemInit<Context
 
         this.player.addListener(new PlayerCallback(this.player) {
             @Override
-            public void onIsPlayingChanged(boolean status) {
+            public void onPlaybackStateChanged(int playbackState) {
                 refresh();
             }
         });
 
-        this.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (player.getPlayer().isPlaying()) {
-                    player.getPlayer().pause();
-                    return;
-                }
-
-                player.getPlayer().play();
-            }
-        });
+        super.init(context);
     }
 
     @Override
     public void refresh() {
         this.setImageDrawable(ContextCompat.getDrawable(this.getContext(), this.player.isPlaying() ? R.drawable.pause : R.drawable.play));
+
+        super.init(this.getContext());
     }
 }
