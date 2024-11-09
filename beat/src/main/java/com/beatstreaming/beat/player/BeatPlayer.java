6f104669fde32ce32f5d53b38d7376d5e86b7f40
Ailakks <hola@ailakks.dev>
2024-11-09 16:02:33 +0100

    feat: beat player media source factory

--- beat/src/main/java/com/beatstreaming/beat/player/BeatPlayer.java
+++ beat/src/main/java/com/beatstreaming/beat/player/BeatPlayer.java
@@ -2,8 +2,13 @@ package com.beatstreaming.beat.player;
 
 import android.content.Context;
 
+import androidx.media3.datasource.okhttp.OkHttpDataSource;
+import androidx.media3.exoplayer.ExoPlayer;
+import androidx.media3.exoplayer.source.DefaultMediaSourceFactory;
+
 import com.beatstreaming.media.player.Player;
 import com.beatstreaming.music.entity.TrackEntity;
+import com.beatstreaming.youtube.player.YouTubePlayerHttpClient;
 
 public class BeatPlayer extends Player<TrackEntity> {
     public BeatPlayer(Context context) {
@@ -12,6 +17,10 @@ public class BeatPlayer extends Player<TrackEntity> {
 
     @Override
     public void onInit() {
+        this.player = new ExoPlayer.Builder(this.context)
+                .setMediaSourceFactory(new DefaultMediaSourceFactory(new OkHttpDataSource.Factory(new YouTubePlayerHttpClient())))
+                .build();
+
         super.onInit();
     }
 }
