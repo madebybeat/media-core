aeda071408efc28c8019504da647d46538f5b34a
Ailakks <hola@ailakks.dev>
2024-11-09 17:19:00 +0100

    fix: music youtube player interceptor mimetype check

--- youtube/src/main/java/com/beatstreaming/youtube/player/MusicYouTubePlayerInterceptor.java
+++ youtube/src/main/java/com/beatstreaming/youtube/player/MusicYouTubePlayerInterceptor.java
@@ -5,6 +5,6 @@ import java.util.Arrays;
 public class MusicYouTubePlayerInterceptor extends YouTubePlayerInterceptor {
     @Override
     public String getMediaUrl() {
-        return Arrays.stream(this.youTubePlayerResponse.getStreamingData().getAdaptiveFormats()).filter((format) -> format.getMimeType().equals("audio/mp4; codecs=\\\"mp4a.40.5\\\"")).findAny().orElseThrow().getUrl();
+        return Arrays.stream(this.youTubePlayerResponse.getStreamingData().getAdaptiveFormats()).filter((format) -> format.getMimeType().equals("audio/mp4; codecs=\"mp4a.40.5\"")).findAny().orElseThrow().getUrl();
     }
 }
