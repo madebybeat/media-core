12fdc5be17776bb0596418f1ea1c7e1a889548cb
Ailakks <hola@ailakks.dev>
2024-11-24 21:48:37 +0100

    fix: player track sheet

--- media/src/main/java/com/beatstreaming/media/player/ui/PlayerMenuButton.java
+++ media/src/main/java/com/beatstreaming/media/player/ui/PlayerMenuButton.java
@@ -9,6 +9,7 @@ import androidx.appcompat.widget.AppCompatImageButton;
 import com.beatstreaming.core.view.ItemInit;
 import com.beatstreaming.media.player.Player;
 import com.beatstreaming.media.sheet.MediaListSheet;
+import com.beatstreaming.media.sheet.MediaSheetContext;
 import com.beatstreaming.media.storage.library.ItemType;
 
 import javax.inject.Inject;
@@ -47,7 +48,7 @@ public class PlayerMenuButton extends AppCompatImageButton implements ItemInit<C
             @Override
             @SneakyThrows
             public void onClick(View view) {
-                mediaListSheet.getConstructor(Context.class).newInstance(view.getContext()).setup(player.getCurrent().getLibraryItem(player.getPlayContext(), itemType)).show();
+                mediaListSheet.getConstructor(Context.class).newInstance(view.getContext()).setup(new MediaSheetContext(null, player.getCurrent().getLibraryItem(player.getPlayContext(), itemType))).show();
             }
         });
     }
